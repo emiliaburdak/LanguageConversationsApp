08869e61c8622638e227a0bceaf76840147146c0
Em <emiliaburdak@gmail.com>
2023-08-22 14:48:54 +0700

    endpoits, continue_this_conversations and existing_conversations has been created

--- app/controller.py
+++ app/controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, make_response
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
-from models import User, Conversation, Message
+from .models import User, Conversation, Message
 from . import db
 import openai
 
@@ -15,10 +15,29 @@ def get_user_id_by_token_identify():
     return user_id
 
 
+def find_all_conversations_names():
+    all_existing_conv_names_tuples = Conversation.query.with_entities(Conversation.conversation_name).all()
+    all_existing_conv_names = [conversation_name[0] for conversation_name in all_existing_conv_names_tuples]
+    return all_existing_conv_names
+
+
+def find_all_conversation_id():
+    all_existing_conv_id_tuples = Conversation.query.with_entities(Conversation.id).all()
+    all_existing_conv_id = [conversation_id[0] for conversation_id in all_existing_conv_id_tuples]
+    return all_existing_conv_id
+
+
+def find_conversation_by_conversation_id(conversation_id):
+    conversation_object = Conversation.query.filter_by(id=conversation_id).first()
+    if not conversation_object:
+        return jsonify({'error': 'Conversation not found'}), 404
+    return conversation_object
+
+
 @controller.route('/home', methods=['GET'])
 @jwt_required()
 def home():
-    username = get_jwt_identity() # fetch actual useraname of user that is invoking this method
+    username = get_jwt_identity()  # fetch actual useraname of user that is invoking this method
     return jsonify({'message': 'Welcome to home!', 'username': username})
 
 
@@ -35,17 +54,33 @@ def create_new_conversation():
     db.session.add(new_conversation)
     db.session.commit()
 
-    return jsonify({'new_conversation': new_conversation})
+    return jsonify({'name': new_conversation.conversation_name, 'id': new_conversation.id})
+
+
+@controller.route('/existing_conversations', methods=['GET'])
+@jwt_required()
+def existing_conversations():
+    all_existing_conv_names = find_all_conversations_names()
+    all_existing_conv_id = find_all_conversation_id()
+    return jsonify({'names': all_existing_conv_names, 'ids': all_existing_conv_id})
+
+
+@controller.route('/continue_this_conversations/<conversation_id>', methods=['GET'])
+@jwt_required()
+def continue_this_conversations(conversation_id):
+    conversation_object = find_conversation_by_conversation_id(conversation_id)
+    all_conversation_messages = conversation_object.messages
+    messages_lies_text_id_time = [[message.message_text, message.id, message.timestamp] for message in all_conversation_messages]
+    return jsonify({'message_info': messages_lies_text_id_time})
 
 
 @controller.route('/speak/<conversation_id>', methods=['POST'])
 @jwt_required()
 def speak(conversation_id):
-
     # save to database stt
 
     data_from_stt = request.get_json()
-    # assume that this json looks like this: {stt_message='blabla', language='spanish', conversation_name= 'dupal123'}
+    # assume that this json looks like this: {TTS_message='blabla'}
     stt_message_text = data_from_stt['TTS_message']
     new_message_stt = Message(message_text=stt_message_text, conversation_id=conversation_id, is_user=True)
 
@@ -54,11 +89,10 @@ def speak(conversation_id):
 
     # API
 
-    conversation_object = Conversation.query.filter_by(conversation_id=conversation_id).first()
-    if not conversation_object:
-        return jsonify({'error': 'Conversation not found'}), 404
+    conversation_object = find_conversation_by_conversation_id(conversation_id)
 
     all_conversation_messages = conversation_object.messages
+    language = conversation_object.language
     last_messages = all_conversation_messages[-4:] if len(all_conversation_messages) > 4 else all_conversation_messages
 
     # User response with last 4 messages for context
@@ -66,7 +100,8 @@ def speak(conversation_id):
                         message in last_messages]
 
     # instruction for chat
-    instruction = {"role": "system", "content": "You are a conversational assistant. Provide short, concise answers and ask follow-up questions to keep the conversation engaging."}
+    instruction = {"role": "system",
+                   "content": f"You are a conversational assistant that speak in {language}. Provide short, concise answers and ask follow-up questions to keep the conversation engaging. adapt the level of difficulty of your speech to your conversation partner"}
 
     # full info for chat
     messages_for_api.insert(0, instruction)
@@ -90,6 +125,3 @@ def speak(conversation_id):
     db.session.commit()
 
     return jsonify({'chat_message': chat_message_text})
-
-
-
