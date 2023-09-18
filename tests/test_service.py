2d87e09022b6ab2e7e1ada3473e6e4ccc544aa9b
Em <emiliaburdak@gmail.com>
2023-09-18 16:35:30 +0800

    complete tests to call_chat_response and prepare_messages has been added

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,13 +1,18 @@
 import json
 import unittest
+from unittest.mock import patch
+
 from flask_testing import TestCase
 from werkzeug.security import generate_password_hash
 from flask_jwt_extended import verify_jwt_in_request
 from app.service import find_all_conversations_names_ids, get_user_id_by_token_identify, \
-    find_conversation_by_conversation_id, save_message_to_database, prepare_api_payload, message_for_api
+    find_conversation_by_conversation_id, save_message_to_database, prepare_api_payload, message_for_api, \
+    prepare_messages, call_chat_response
 from app import db
 from app.models import User, Conversation, Message
 from main import app
+from app.controller import ChatAPIError
+
 
 
 class ServiceTests(TestCase):
@@ -45,7 +50,6 @@ class ServiceTests(TestCase):
 
         # "Hey Flask, I want to simulate an HTTP request for this app."
         with self.app.test_request_context(headers={"Authorization": f"Bearer {bearer_token}"}):
-
             # if you want to manually ensure a JWT's presence
             verify_jwt_in_request()
 
@@ -118,6 +122,65 @@ class ServiceTests(TestCase):
         self.assertEqual(formatted_messages, [{"role": "system",
                                                "content": f"You must return your whole response in JSON. You're a chat assistant fluent in Spanish. Always begin with a summary sentence (max 15 words). Then, provide a concise answer or question (max 10 words) related to sentence. \n \n It's crucial to respond ONLY in format with key such as summary and answer. Only JSON allowed"},
                                               {"role": "user", "content": "Test"}])
+
+    def test_prepare_messages(self):
+        self.add_conversation_and_message()
+        last_message, summary = prepare_messages(1)
+        self.assertEqual(last_message, "Hello Again")
+        self.assertEqual(summary, None)
+
+    def test_prepare_messages_no_messages(self):
+        conversation1 = Conversation(conversation_name="Test Conversation 1", user_id=self.test_user.id,
+                                     language="Spanish")
+        db.session.add(conversation1)
+        db.session.commit()
+        self.assertRaises(ValueError, lambda: prepare_messages(1))
+
+    def test_call_chat_response_hint_message(self):
+        summary = "testing"
+        last_message = "testing this function"
+        hint_message = [{"role": "user",
+                             "content": f"{summary}, give me only one sentence example answer to this '{last_message}'"}]
+        mock_response = {
+            "choices": [
+                {
+                    "message": {
+                        "content": f"mock response"
+                    }
+                }
+            ]
+        }
+        with patch("openai.ChatCompletion.create", return_value=mock_response):
+            hint_response = call_chat_response(hint_message)
+            self.assertEqual(hint_response, "mock response")
+
+
+    def test_call_chat_response_advance_version(self):
+        summary = "testing"
+        last_message = "testing this function"
+        user_attempt_message = "test if you can make this sentence better"
+        advance_version_message = [{"role": "user",
+                             "content": f"{summary}, this is last message '{last_message}', transform this '{user_attempt_message}' to make it more linguistically advanced"}]
+        mock_response = {
+            "choices": [
+                {
+                    "message": {
+                        "content": f"mock response"
+                    }
+                }
+            ]
+        }
+        with patch("openai.ChatCompletion.create", return_value=mock_response):
+            advance_version_response = call_chat_response(advance_version_message)
+            self.assertEqual(advance_version_response, "mock response")
+
+    def test_call_chat_response_error(self):
+        summary = "testing"
+        last_message = "testing this function"
+        guidance_message = [{"role": "user",
+                             "content": f"{summary}, give me only one sentence example answer to this '{last_message}'"}]
+        with patch("openai.ChatCompletion.create", side_effect=ChatAPIError("Failed to get a response from the chat")):
+            self.assertRaises(ChatAPIError, lambda: call_chat_response(guidance_message))
 
 
 if __name__ == "__main__":
